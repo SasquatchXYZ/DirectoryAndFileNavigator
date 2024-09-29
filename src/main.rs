fd5d8191faa9606a4a9f8b520c674e7d2f6dcd9d
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-09-29 17:33:42 -0400

    Setup and first code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut current_dir = std::env::current_dir().unwrap();
+    println!("Current director: {current_dir:?}");
+
+    let mut read_dir = std::fs::read_dir(&current_dir).unwrap();
+    println!("{read_dir:?}");
+    let first = read_dir.nth(1).unwrap().unwrap();
+    println!("Path: {:?} Name: {:?}", first.path(), first.file_name());
+
+    current_dir.pop();
+    println!("Now moved back to: {current_dir:?}");
+
+    let mut read_dir = std::fs::read_dir(&current_dir).unwrap();
+    println!("{read_dir:?}");
+    let first = read_dir.nth(1).unwrap().unwrap();
+    println!("Path: {:?} Name: {:?}", first.path(), first.file_name());
 }
